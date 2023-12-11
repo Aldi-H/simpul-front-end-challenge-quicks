b13a86c18691c3adabc76564647e7e2fa268c922
Aldi-H <aldihimawan88@gmail.com>
2023-12-11 19:43:08 +0700

    style: add icon

---
+++ src/components/icons/Icons.tsx
@@ -0,0 +1,213 @@
+"use client";
+
+import { createSvgIcon } from "@mui/material";
+
+export const CloudLightning = createSvgIcon(
+  <svg
+    width="56"
+    height="56"
+    viewBox="0 0 56 56"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M31.4427 12.3359C32.3618 12.9486 32.6101 14.1903 31.9974 15.1094L24.737 26H35C35.7376 26 36.4153 26.4059 36.7634 27.0563C37.1114 27.7066 37.0732 28.4957 36.6641 29.1094L27.3308 43.1094C26.7181 44.0284 25.4763 44.2768 24.5573 43.6641C23.6382 43.0514 23.3899 41.8096 24.0026 40.8906L31.263 30H21C20.2624 30 19.5847 29.594 19.2367 28.9437C18.8886 28.2934 18.9268 27.5043 19.3359 26.8906L28.6692 12.8906C29.2819 11.9715 30.5237 11.7232 31.4427 12.3359Z"
+      fill="currentColor"
+    />
+  </svg>,
+  "CloudLightning",
+);
+
+export const InboxDefaultOutline = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M20.6433 2.51453H4.29826C3.60674 2.51453 3.04095 3.08032 3.04095 3.77184V21.3742L8.07019 16.3449H20.6433C21.3348 16.3449 21.9006 15.7791 21.9006 15.0876V3.77184C21.9006 3.08032 21.3348 2.51453 20.6433 2.51453ZM19.386 5.02908V13.8302H7.02665L6.28484 14.5721L5.5556 15.3013V5.02908H19.386ZM24.4152 7.54381H26.9298C27.6213 7.54381 28.1871 8.10959 28.1871 8.80112V27.6608L23.1579 22.6315H9.32748C8.63596 22.6315 8.07017 22.0657 8.07017 21.3742V18.8596H24.4152V7.54381Z"
+      fill="currentColor"
+      // fill="white"
+    />
+  </svg>,
+  "InboxDefaultOutline",
+);
+
+export const InboxColoredOutline = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M20.8187 2.51453H4.47368C3.78216 2.51453 3.21637 3.08032 3.21637 3.77184V21.3742L8.24561 16.3449H20.8187C21.5102 16.3449 22.076 15.7791 22.076 15.0876V3.77184C22.076 3.08032 21.5102 2.51453 20.8187 2.51453ZM19.5614 5.02908V13.8302H7.20201L6.46019 14.5721L5.73095 15.3013V5.02908H19.5614ZM24.5907 7.54381H27.1053C27.7968 7.54381 28.3626 8.10959 28.3626 8.80112V27.6608L23.3334 22.6315H9.50296C8.81144 22.6315 8.24565 22.0657 8.24565 21.3742V18.8596H24.5907V7.54381Z"
+      // fill="currentColor"
+      fill="#8885FF"
+    />
+  </svg>,
+  "InboxColoredOutline",
+);
+
+export const ToDoDefaultOutline = createSvgIcon(
+  <svg
+    width="32"
+    height="31"
+    viewBox="0 0 32 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4.70175 4.40063H27.3333C28.7164 4.40063 29.848 5.53221 29.848 6.91526V23.2603C29.848 24.6433 28.7164 25.7749 27.3333 25.7749H4.70175C3.31871 25.7749 2.18713 24.6433 2.18713 23.2603V6.91526C2.18713 5.53221 3.31871 4.40063 4.70175 4.40063ZM4.70172 6.91528V23.2603H14.7602V6.91528H4.70172ZM27.3333 23.2603H17.2749V6.91528H27.3333V23.2603ZM26.0761 11.3158H18.5322V13.2018H26.0761V11.3158ZM18.5322 14.4591H26.0761V16.3451H18.5322V14.4591ZM26.0761 17.6024H18.5322V19.4883H26.0761V17.6024Z"
+      fill="currentColor"
+      // fill="white"
+    />
+  </svg>,
+  "ToDoDefaultOutline",
+);
+
+export const ToDoColoredOutline = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.92984 4.40063H26.5614C27.9445 4.40063 29.076 5.53221 29.076 6.91526V23.2603C29.076 24.6433 27.9445 25.7749 26.5614 25.7749H3.92984C2.5468 25.7749 1.41522 24.6433 1.41522 23.2603V6.91526C1.41522 5.53221 2.5468 4.40063 3.92984 4.40063ZM3.9298 6.91528V23.2603H13.9883V6.91528H3.9298ZM26.5614 23.2603H16.5029V6.91528H26.5614V23.2603ZM25.3042 11.3158H17.7603V13.2018H25.3042V11.3158ZM17.7603 14.4591H25.3042V16.3451H17.7603V14.4591ZM25.3042 17.6024H17.7603V19.4883H25.3042V17.6024Z"
+      fill="#F8B76B"
+    />
+  </svg>,
+
+  "ToDoColoredOutline",
+);
+
+export const MoreHorizontal = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8.07016 12.5731C6.68712 12.5731 5.55554 13.7046 5.55554 15.0877C5.55554 16.4707 6.68712 17.6023 8.07016 17.6023C9.4532 17.6023 10.5848 16.4707 10.5848 15.0877C10.5848 13.7046 9.4532 12.5731 8.07016 12.5731ZM23.1579 12.5731C21.7748 12.5731 20.6433 13.7046 20.6433 15.0877C20.6433 16.4707 21.7748 17.6023 23.1579 17.6023C24.5409 17.6023 25.6725 16.4707 25.6725 15.0877C25.6725 13.7046 24.5409 12.5731 23.1579 12.5731ZM13.0994 15.0877C13.0994 13.7046 14.231 12.5731 15.614 12.5731C16.9971 12.5731 18.1286 13.7046 18.1286 15.0877C18.1286 16.4707 16.9971 17.6023 15.614 17.6023C14.231 17.6023 13.0994 16.4707 13.0994 15.0877Z"
+      fill="white"
+    />
+  </svg>,
+  "MoreHorizontal",
+);
+
+export const BookmarkColoredOutline = createSvgIcon(
+  <svg
+    width="29"
+    height="31"
+    viewBox="0 0 29 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M23.005 1.25732H11.116C9.80952 1.25732 8.75246 2.3889 8.75246 3.77194H20.6295C21.936 3.77194 23.005 4.90352 23.005 6.28656V22.6316L25.3804 23.8889V3.77194C25.3804 2.3889 24.3114 1.25732 23.005 1.25732ZM18.2543 8.80118V25.1085L13.254 22.8328L12.3157 22.4053L11.3774 22.8328L6.37719 25.1085V8.80118H18.2543ZM6.37712 6.28655H18.2542C19.5607 6.28655 20.6296 7.41813 20.6296 8.80117V28.9181L12.3157 25.1462L4.00171 28.9181V8.80117C4.00171 7.41813 5.07065 6.28655 6.37712 6.28655Z"
+      fill="#2F80ED"
+    />
+  </svg>,
+  "BookmarkColoredOutline",
+);
+
+export const Close = createSvgIcon(
+  <svg
+    width="21"
+    height="21"
+    viewBox="0 0 21 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21 2.115L18.885 0L10.5 8.385L2.115 0L0 2.115L8.385 10.5L0 18.885L2.115 21L10.5 12.615L18.885 21L21 18.885L12.615 10.5L21 2.115Z"
+      fill="#F2F2F2"
+    />
+  </svg>,
+  "close",
+);
+
+export const ArrowBack = createSvgIcon(
+  <svg
+    width="32"
+    height="31"
+    viewBox="0 0 32 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M25.9883 13.8304H10.6868L17.7152 6.80204L15.9298 5.02924L5.87134 15.0877L15.9298 25.1462L17.7026 23.3734L10.6868 16.345H25.9883V13.8304Z"
+      fill="#F2F2F2"
+    />
+  </svg>,
+  "ArrowBack",
+);
+
+export const ArrowDownward = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M25.15 15.09L23.377 13.317L16.3475 20.3338V5.03003H13.8325V20.3338L6.81568 13.3044L5.03003 15.09L15.09 25.15L25.15 15.09Z"
+      fill="#F2F2F2"
+    />
+  </svg>,
+  "ArrowDownward",
+);
+
+export const KeyboardArrowUp = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5.05999 19.3943L6.83307 21.1674L15.12 12.9843L23.3943 21.18L25.18 19.3943L15.12 9.33434L5.05999 19.3943Z"
+      fill="#F2F2F2"
+    />
+  </svg>,
+  "KeyboardArrowUp",
+);
+
+export const KeyboardArrowDown = createSvgIcon(
+  <svg
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M25.12 10.7857L23.3469 9.01258L15.06 17.1957L6.78565 9L5 10.7857L15.06 20.8456L25.12 10.7857Z"
+      fill="#F2F2F2"
+    />
+  </svg>,
+  "KeyboardArrowDown",
+);
